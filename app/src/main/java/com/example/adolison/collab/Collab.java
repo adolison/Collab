532d61b47675172de612e280098109d7a7ed57ee
Gregory Pailet <gpailet@fb.com>
2017-07-07 10:57:24 -0700

    Add color for red feature to color.xml

---
+++ app/src/main/java/com/example/adolison/collab/Collab.java
@@ -0,0 +1,13 @@
+package com.example.adolison.collab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Collab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collab);
+    }
+}
